f196d835dd8ebc718373eef0845a48b4890e88cd
Muhammad Rafay <muhammadrafay151@gmail.com>
2020-09-27 04:49:23 -0700

    initial commit

---
+++ chaincode/ecert/javascript/index.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const ECert = require('./lib/ecert');
+
+module.exports.ECert  = ECert ;
+module.exports.contracts = [ ECert ];
